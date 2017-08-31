3f1f953087df1256002135dc9b1b8af9965fa129
Stephanos Theodotou <stephanostheodotou@Stephanoss-MacBook-Pro.local>
2017-08-31 17:34:51 +0200

    refactor ListContacts Class as a Stateless Functional Component. Pass props as argument and remove un-necessary contextual reference to this.

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,11 +1,10 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class ListContacts extends Component{
-  render(){
-    console.log(this);
+function ListContacts(props){
+
     return (
       <ol className="contact-list">
-        {this.props.contacts.map((contact) => (
+        {props.contacts.map((contact) => (
           <li key={contact.id} className="contact-list-item">
             <div className="contact-avatar" style={{
               backgroundImage:`url(${contact.avatarURL})`
@@ -20,6 +19,5 @@ class ListContacts extends Component{
       </ol>
     )
   }
-}
 
 export default ListContacts;
